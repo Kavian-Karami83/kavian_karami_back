dbf1c0ce5119b05d05167ab35aa64b2eccfa5f6f
kavian.karami <kavian.karami83@gmail.com>
2025-05-04 21:45:17 +0330

    Get model and price

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const axios = require('axios');
+
+const models = 'https://lm-models.s3.ir-thr-at1.arvanstorage.ir/cars.json';
+
+axios.get(models)
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });

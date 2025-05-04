16f601adf0a9865986e561fb27e9528c8270fbfb
kavian.karami <kavian.karami83@gmail.com>
2025-05-04 21:46:35 +0330

    Get the dollar price

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const models = 'https://lm-models.s3.ir-thr-at1.arvanstorage.ir/cars.json';
+const currency = 'https://baha24.com/api/v1/price';
 
 axios.get(models)
     .then(function (response) {
@@ -9,3 +10,15 @@ axios.get(models)
     .catch(function (error) {
         console.log(error);
     });
+
+axios.get(currency)
+    .then(function (response) {
+        console.log(response.data.USD);
+
+
+    })
+    .catch(function (error) {
+        console.log(error);
+    }
+
+    );
